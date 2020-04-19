eb2732459b942194830b76aae4076b85636b256a
Andrey Mileshin <andrej.mileshin@ya.ru>
2020-04-20 02:40:09 +0300

    Added initialization of postgres

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # storage_and_db
 Storage and db labs
+
+Запуск необходимо выполнять с помощью команды `docker-compose up -d --build`. Выключать с помощью `docker-compose down`.
+
+Таблицы и пользователи генерируются автоматически.
+
+На ip 2987 весит adminer, который в той же сети, что и базы данных. Через него можно легко ими управлять(хотя консоль ван лав!).
+
+## Postgres
+В базе данных будет 4 таблицы: 
+    curriculum
+    grades
+    students
+    teachers
---
+++ conf.d/postgres/init.sql
@@ -0,0 +1,52 @@
+CREATE USER andrey WITH PASSWORD 'qwe123';
+CREATE DATABASE report_cards;
+GRANT ALL PRIVILEGES ON DATABASE report_cards TO andrey;
+ALTER USER andrey VALID UNTIL 'infinity';
+
+\c report_cards
+
+CREATE TABLE students (
+   id             int         PRIMARY KEY,
+   FirstName      CHARACTER   VARYING(30)    NOT NULL,
+   LastName       CHARACTER   VARYING(30)    NOT NULL,
+   Patronymic     CHARACTER   VARYING(30),
+   university     CHARACTER   VARYING(110)   NOT NULL,
+   studyStandart  boolean                    NOT NULL,
+   studyForm      boolean                    NOT NULL,
+   faculty        CHARACTER   VARYING(110)   NOT NULL,
+   specialty      CHARACTER   VARYING(110)   NOT NULL
+);
+
+CREATE TABLE teachers (
+   id             int         PRIMARY KEY,
+   FirstName      CHARACTER   VARYING(30)    NOT NULL,
+   LastName       CHARACTER   VARYING(30)    NOT NULL,
+   Patronymic     CHARACTER   VARYING(30),
+   university     CHARACTER   VARYING(110)   NOT NULL
+);
+
+CREATE TABLE Curriculum (
+   id             int         PRIMARY KEY,
+   Subject        CHARACTER   VARYING(30)    NOT NULL,
+   semester       int         CHECK (semester > 0 and semester < 7) NOT NULL,
+   lecture        CHARACTER   VARYING(30)    NOT NULL,
+   practice       CHARACTER   VARYING(30)    NOT NULL,
+   exam           boolean                    NOT NULL
+);
+
+CREATE TABLE grades (
+   idStudent      int         NOT  NULL,
+   idTeacher      int         NOT  NULL,
+   idSubject      int         NOT  NULL,
+   grade          real        CHECK (grade >= 0 and grade <= 100) NOT NULL,
+   date_of_change date 
+);
+CREATE FUNCTION grade_stamp() RETURNS trigger AS $grade_stamp$
+    BEGIN
+        NEW.date_of_change := to_char(current_timestamp, 'yyyy-mm-dd');
+        RETURN NEW;
+    END;
+$grade_stamp$ LANGUAGE plpgsql;
+
+CREATE TRIGGER grade_stamp BEFORE INSERT OR UPDATE ON grades
+    FOR EACH ROW EXECUTE PROCEDURE grade_stamp();
