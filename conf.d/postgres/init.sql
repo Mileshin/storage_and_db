4813eb4bb4a6c6cdc4e3e8f1aab4edba02b97174
Andrey Mileshin <andrej.mileshin@ya.ru>
2020-04-20 16:15:10 +0300

    Added REFERENCES to postgres

--- conf.d/postgres/init.sql
+++ conf.d/postgres/init.sql
@@ -25,7 +25,7 @@ CREATE TABLE teachers (
    university     CHARACTER   VARYING(110)   NOT NULL
 );
 
-CREATE TABLE Curriculum (
+CREATE TABLE curriculum (
    id             int         PRIMARY KEY,
    Subject        CHARACTER   VARYING(30)    NOT NULL,
    semester       int         CHECK (semester > 0 and semester < 7) NOT NULL,
@@ -35,12 +35,14 @@ CREATE TABLE Curriculum (
 );
 
 CREATE TABLE grades (
-   idStudent      int         NOT  NULL,
-   idTeacher      int         NOT  NULL,
-   idSubject      int         NOT  NULL,
+   idStudent      int         NOT  NULL   REFERENCES "students" ("id"),
+   idTeacher      int         NOT  NULL   REFERENCES "teachers" ("id"),
+   idSubject      int         NOT  NULL   REFERENCES "curriculum" ("id"),
    grade          real        CHECK (grade >= 0 and grade <= 100) NOT NULL,
-   date_of_change date 
+   date_of_change date,
+   PRIMARY KEY (idStudent,idTeacher,idSubject)
 );
+
 CREATE FUNCTION grade_stamp() RETURNS trigger AS $grade_stamp$
     BEGIN
         NEW.date_of_change := to_char(current_timestamp, 'yyyy-mm-dd');
