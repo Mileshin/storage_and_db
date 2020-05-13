e8f069fae7f36107c7a4060eb0a049b22b477275
Kamilla Magomedova <m.k.1296@yandex.ru>
2020-05-13 03:04:15 +0300

    Modified mongo init.js

--- docker/conf.d/mongo/init.js
+++ docker/conf.d/mongo/init.js
@@ -16,21 +16,59 @@ db.createCollection("lodger")
 db.createCollection("attendance")
 
 db.people.insert({
-      id:             207209,
-      firstName:      "Andrey",
-      lastName:       "Mileshin",
-      patronomic:     "Aleksandrovich",
-      dateOfBirth:    "08-03-1997",
-      placeOfBirth:   "Saransk"
+      id:             0,
+      firstName:      "Ivan",
+      lastName:       "Ivanov",
+      patronomic:     "Ivanovich",
+      dateOfBirth:    "1980-07-04",
+      placeOfBirth:   "London"
       benefit:        "FALSE",
       educationType:  "budget"
 })
 
+db.people.insert({
+{
+	  id:             1,
+      firstName:      "Sergey",
+      lastName:       "Ivanov",
+      patronomic:     "Ivanovich",
+      dateOfBirth:    "1980-07-04",
+      placeOfBirth:   "London"
+      benefit:        "FALSE",
+      educationType:  "budget"
+})
+
+db.people.insert({
+{
+	  id:             2,
+      firstName:      "Alisa",
+      lastName:       "Sergeevna",
+      patronomic:     "Yandex",
+      dateOfBirth:    "2015-07-04",
+      placeOfBirth:   "Moskva"
+      benefit:        "FALSE",
+      educationType:  "budget"
+	}
+)
+
+db.people.insert({
+{
+	  id:             3,
+      firstName:      "Ochen",
+      lastName:       "Umniy",
+      patronomic:     "Chelovek",
+      dateOfBirth:    "1960-07-04",
+      placeOfBirth:   "Ladoga"
+      benefit:        "FALSE",
+      educationType:  "budget"
+	}
+)
+
 db.rooms.insert(
    {
-      roomNumber:     "1431-3",
-      roomSize:       "three persons",
-      numLodgers:     1,
+      roomNumber:     "1101-1",
+      roomSize:       "3 persons",
+      numLodgers:     3,
       disinfection:   "2019-12-12",
       bedbugs:        "FALSE"
    }
@@ -38,10 +76,20 @@ db.rooms.insert(
 
 db.rooms.insert(
    {
-      roomNumber:     "1431-3",
+      roomNumber:     "1221-2",
       roomSize:       "3 persons",
       numLodgers:     1,
-      disinfection:   "2019-12-12",
+      disinfection:   "2019-11-12",
+      bedbugs:        "FALSE"
+   }
+)
+
+db.rooms.insert(
+   {
+      roomNumber:     "1323-1",
+      roomSize:       "3 persons",
+      numLodgers:     2,
+      disinfection:   "2019-12-10",
       bedbugs:        "FALSE"
    }
 )
@@ -62,19 +110,76 @@ db.dormitories.insert(
 
 db.lodger.insert(
    {
-      lodger:         207209,
-      roomNumber:     "1431-3",
+      lodger:         0,
+      roomNumber:     "1101-1",
+      rebuke:         "No",
+      residenceStart: "2002-09-01",
+      residenceEnd:   "2004-08-31",
+      paymentAmount:  1200
+   }
+)
+
+db.lodger.insert(
+   {
+	  lodger:         1,
+      roomNumber:     "1101-1",
+      rebuke:         "No",
+      residenceStart: "2002-09-01",
+      residenceEnd:   "2004-08-31",
+      paymentAmount:  1200
+   }
+)
+
+db.lodger.insert(
+   {
+	  lodger:         2,
+      roomNumber:     "1221-2",
       rebuke:         "No",
       residenceStart: "2015-09-01",
-      residenceEnd:   "2021-08-31",
+      residenceEnd:   "2016-08-31",
+      paymentAmount:  1500
+   }
+)
+
+db.lodger.insert(
+   {
+	  lodger:         3,
+      roomNumber:     "1323-1",
+      rebuke:         "No",
+      residenceStart: "2015-09-01",
+      residenceEnd:   "2016-08-31",
       paymentAmount:  1500
    }
 )
 
 db.attendance.insert(
    {
-      lodger:        207209,
-      time:          "2020-22-04 22:20:15",
-      attendance:    "entered"
+      lodger:        0,
+      time:          "2004-07-04 22:20:15",
+      attendance:    "left"
+   }
+)
+
+db.attendance.insert(
+   {
+	  lodger:        1,
+      time:          "2004-07-04 22:21:14",
+      attendance:    "left"
+   }
+)
+
+db.attendance.insert(
+   {
+      lodger:        2,
+      time:          "2015-07-04 22:50:05",
+      attendance:    "left"
+   }
+)
+
+db.attendance.insert(
+   {
+      lodger:        3,
+      time:          "2019-07-04 12:30:04",
+      attendance:    "arrived"
    }
 )
